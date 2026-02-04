8cd2caa367a5bf135f1c10742b1be1984a173664
QSMEmmanuel <eflores@qsmsemiconductores.com>
2026-02-04 15:19:10 -0600

    Add files via upload
    Subiendo el circuito

---
+++ src/Sumador8b.v
@@ -0,0 +1,20 @@
+
+module sumador8b (
+    input wire clk,
+    input wire rst,
+    input wire enable,
+    output reg cout,
+    output reg [7:0] out
+);
+
+    always @(posedge clk or negedge rst) begin
+        if(!rst)
+        begin
+            out=8'd0;
+            cout=1'b0;
+        end
+        else if(enable)
+            {cout,out}={cout,out}+9'd1 
+    end
+    
+endmodule
