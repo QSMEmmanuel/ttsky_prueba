de3d138fc5a7b09f6326b3821de636642ac11283
QSMEmmanuel <eflores@qsmsemiconductores.com>
2026-02-04 16:59:56 -0600

    Fix syntax errors in sumador8b module

--- src/sumador8b.v
+++ src/sumador8b.v
@@ -14,7 +14,8 @@ module sumador8b (
             cout=1'b0;
         end
         else if(enable)
-            {cout,out}={cout,out}+9'd1 
+        {cout,out}={cout,out}+9'd1;
     end
     
 endmodule
+
