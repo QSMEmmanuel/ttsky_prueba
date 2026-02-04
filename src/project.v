6dcdfd0d4991438744127006d08fb1eea38bb84c
QSMEmmanuel <eflores@qsmsemiconductores.com>
2026-02-04 15:39:47 -0600

    Refactor tt_um_example with sumador8b integration
    Refactor the tt_um_example module to include a sumador8b instance and adjust output assignments.

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_sumador8b (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,11 +17,27 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+ // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  assign uio_out[6:0] = 7'b000_0000;
+  assign uio_oe  = 8'b1000_0000;
+
+wire enable;
+wire[7:0];
+wire cout;
+
+assign enable = ui_in[0]; //Conecto el prime enable de los inputs.
+assign uo_out[7:0] = out; //conecto la salida a los pones de salida.
+assign uio_out[7] = cout;
+
+sumador8b sumador8b_Unit(
+  .clk(clk),
+  .rst(rst_n),
+  .enable(enable),
+  .out(out),
+  .cout(cout)
+);
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in[7:0],ui_in[7:1], 1'b0};
 
 endmodule
